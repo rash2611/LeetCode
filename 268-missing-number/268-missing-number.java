9ce056e6326ef5dc572e33893d5c1b3dc1468ce4
rash2611 <rashi.smita2611@gmail.com>
2022-09-02 13:17:14 +0530

    Time: 1 ms (78.38%), Space: 50.7 MB (78.30%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -3,12 +3,13 @@ class Solution {
         int xorValue = 0;
         for(int index = 0; index<nums.length;index++)
         {
-            xorValue = xorValue ^ nums[index];
+            xorValue = xorValue ^ nums[index] ^ index;
         }
-        for(int i = 0;i<=nums.length;i++)
-        {
-            xorValue = xorValue^i;
-        }
-        return xorValue;
+        // for(int i = 0;i<=nums.length;i++)
+        // {
+        //     xorValue = xorValue^i;
+        // }
+        
+        return xorValue^nums.length;
     }
 }
