181a47b2970c088c70e4f9557b48dd67658dc0d9
rash2611 <rashi.smita2611@gmail.com>
2022-09-03 17:21:35 +0530

    Time: 2 ms (65.03%), Space: 49.6 MB (84.79%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,11 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int unique = 0;
-        for(int i = 0;i<nums.length;i++)
+        int xorValue = 0;
+        for(int index = 0;index < nums.length; index++)
         {
-            unique = unique ^ nums[i];
+            xorValue = xorValue ^ nums[index];
         }
-        return unique;
-        
+        return xorValue;
     }
 }
