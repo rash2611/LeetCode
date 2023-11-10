dbcd23871a07dc4d137a095cec4b073c8e24f0c0
Rashi Smita <rashi.smita2611@gmail.com>
2023-11-10 19:49:27 +0530

    Time: 15 ms (15.39%) | Memory: 46.5 MB (86.64%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,7 +1,13 @@
 import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<nums.length;i++)
+        {
+            map.put(nums[i], map.getOrDefault(nums[i],0) + 1);
+            if(map.get(nums[i]) > nums.length/2)
+                return nums[i];
+        }
+        return -1;
     }
 }
