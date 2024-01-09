f509722356049f1258ad46041917acdbb953b7b5
Rashi Smita <rashi.smita2611@gmail.com>
2024-01-10 03:03:19 +0530

    Time: 1 ms (30.75%) | Memory: 41.7 MB (24.56%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,15 +1,4 @@
 class Solution {
-    public int f(int dp[][], int[][] obstacleGrid, int i,int j){
-        if(i == 0 && j == 0 && obstacleGrid[i][j]!=1)
-            return dp[i][j] = 1;
-        if(i<0 || j<0 || obstacleGrid[i][j] == 1)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int left = f(dp,obstacleGrid, i, j-1);
-        int up = f(dp,obstacleGrid, i-1,j);
-        return dp[i][j] = left+up;
-    }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
