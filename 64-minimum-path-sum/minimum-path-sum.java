e8bd9906bece63f311c65588cc7f623fa0a49b83
Rashi Smita <rashi.smita2611@gmail.com>
2024-01-10 17:03:07 +0530

    Time: 3 ms (65.93%) | Memory: 45.6 MB (17.61%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,25 +3,27 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int dp[][] = new int[m][n];
+        int prev[] = new int[n];
         for(int i = 0;i<m;i++)
         {
+            int temp[] = new int[n];
             for(int j = 0;j<n;j++)
             {
                 if(i == 0 && j == 0)
-                    dp[i][j] = grid[i][j];
+                    temp[j] = grid[i][j];
                 else if(i == 0)
-                    dp[i][j] = grid[i][j] + dp[i][j-1];
+                    temp[j] = grid[i][j] + temp[j-1];
                 else if(j == 0)
-                    dp[i][j] = grid[i][j] + dp[i-1][j];
+                    temp[j] = grid[i][j] + prev[j];
                 else
                 {
-                    int up = dp[i-1][j];
-                    int left = dp[i][j-1];
-                    dp[i][j] = grid[i][j] + Math.min(left,up);
+                    int up = prev[j];
+                    int left = temp[j-1];
+                    temp[j] = grid[i][j] + Math.min(left,up);
                 }
             }
+            prev = temp;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 }
