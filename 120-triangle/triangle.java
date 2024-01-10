43a07233c01400aa8045612bdfd2912a2ca696e5
Rashi Smita <rashi.smita2611@gmail.com>
2024-01-11 01:44:39 +0530

    Time: 3 ms (70.12%) | Memory: 44 MB (75.61%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,19 +1,17 @@
 class Solution {
-    public int minimumTotalHelper(List<List<Integer>> triangle, int i, int j, int n, int[][] dp)
-    {
-        if(i == n-1)
-            return dp[i][j] = triangle.get(i).get(j);
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int down = minimumTotalHelper(triangle,i+1,j,n,dp);
-        int diagonal = minimumTotalHelper(triangle,i+1,j+1,n,dp);
-        return dp[i][j] = triangle.get(i).get(j) + Math.min(down,diagonal);
-    }
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         int dp[][] = new int[n][n];
-        for(int[] row : dp)
-            Arrays.fill(row,-1);
-        return minimumTotalHelper(triangle, 0, 0, n, dp);
+        for(int i = n-1;i>=0;i--)
+        {
+            for(int j = i;j>=0;j--)
+            {
+                if(i == n-1)
+                    dp[i][j] = triangle.get(i).get(j);
+                else
+                    dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 }
