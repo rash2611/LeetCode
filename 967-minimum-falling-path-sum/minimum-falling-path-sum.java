bc83186eac9d5c394fd9c3f0778594cefd2d44f6
Rashi Smita <rashi.smita2611@gmail.com>
2024-01-12 16:12:36 +0530

    Time: 6 ms (67.39%) | Memory: 45.2 MB (33.58%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,30 +1,32 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
-        int dp[][] = new int[n][n];
+        int front[] = new int[n];
         int minSum = Integer.MAX_VALUE;
         for(int i = n-1;i>=0;i--)
         {
+            int curr[] = new int[n];
             for(int j = n-1;j>=0;j--)
             {
                 if(i == n-1)
                 {
-                    dp[i][j] = matrix[i][j];
+                    curr[j] = matrix[i][j];
                 }
                 else{
-                    int down = dp[i+1][j];
+                    int down = front[j];
                     int leftDiagonal = Integer.MAX_VALUE;
                     int rightDiagonal = Integer.MAX_VALUE;
                     if(j>0)
-                        leftDiagonal = dp[i+1][j-1];
+                        leftDiagonal = front[j-1];
                     if(j<n-1)
-                        rightDiagonal = dp[i+1][j+1];
-                    dp[i][j] = matrix[i][j] +  Math.min(down, Math.min(leftDiagonal,rightDiagonal));
+                        rightDiagonal = front[j+1];
+                    curr[j] = matrix[i][j] +  Math.min(down, Math.min(leftDiagonal,rightDiagonal));
                     
                 }
                 if(i == 0)
-                        minSum = Math.min(minSum, dp[i][j]);
+                    minSum = Math.min(minSum, curr[j]);
             }
+            front = curr;
         }
         return minSum;
     }
