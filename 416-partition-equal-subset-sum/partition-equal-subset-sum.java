57089678efa172cb8ea12061d8ae7362905caf0d
Rashi Smita <rashi.smita2611@gmail.com>
2024-01-15 18:44:37 +0530

    Time: 38 ms (67.99%) | Memory: 45 MB (76.97%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -11,25 +11,25 @@ class Solution {
         else
         {
             int k = totSum/2;
-            boolean dp[][] = new boolean[n][k+1];
-            for(int i = 0;i<n;i++)
-            {
-                dp[i][0] = true;
-            }
+            boolean prev[] = new boolean[k+1];
+            prev[0] = true;
             if(nums[0]<=k)
-                dp[0][nums[0]] = true;
+                prev[nums[0]] = true;
             for(int i = 1;i<n;i++)
             {
+                boolean curr[] = new boolean[k+1];
+                curr[0] = true;
                 for(int target = 1;target<=k;target++)
                 {
-                    boolean notTaken = dp[i-1][target];
+                    boolean notTaken = prev[target];
                     boolean taken = false;
                     if(nums[i] <= target)
-                        taken = dp[i-1][target-nums[i]];
-                    dp[i][target] = taken || notTaken;
+                        taken = prev[target-nums[i]];
+                    curr[target] = taken || notTaken;
                 }
+                prev = curr;
             }
-            return dp[n-1][k];
+            return prev[k];
        }
     }
 }
