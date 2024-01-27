563c5c0b5959742c0a047616a275ae81cb813d43
Rashi Smita <rashi.smita2611@gmail.com>
2024-01-28 00:43:38 +0530

    Time: 71 ms (64.67%) | Memory: 42.7 MB (33.13%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -1,18 +1,4 @@
 class Solution {
-    public int getMaxCoins(int i,int j,ArrayList<Integer> list, int[][] dp)
-    {
-        if(i>j)
-            return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        int maxi = Integer.MIN_VALUE;
-        for(int k = i;k<=j;k++)
-        {
-            int ans = list.get(i-1) * list.get(k) * list.get(j+1) + getMaxCoins(i,k-1,list,dp) + getMaxCoins(k+1,j,list,dp);
-            maxi = Math.max(maxi,ans); 
-        }
-        return dp[i][j] = maxi;
-    }
     public int maxCoins(int[] nums) {
         int n = nums.length;
         ArrayList<Integer> list = new ArrayList<>();
@@ -20,9 +6,22 @@ class Solution {
         for(int num : nums)
             list.add(num);
         list.add(1);
-        int[][] dp = new int[n+1][n+1];
-        for(int[] row: dp)
-            Arrays.fill(row,-1);
-        return getMaxCoins(1,n,list,dp);
+        int[][] dp = new int[n+2][n+2];
+        for(int i = n;i>=1;i--)
+        {
+            for(int j = 1;j<=n;j++)
+            {
+                if(i>j)
+                    continue;
+                int maxi = Integer.MIN_VALUE;
+                for(int k = i;k<=j;k++)
+                {
+                    int ans = list.get(i-1) * list.get(k) * list.get(j+1) + dp[i][k-1] + dp[k+1][j];
+                    maxi = Math.max(maxi,ans);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][n];
     }
 }
