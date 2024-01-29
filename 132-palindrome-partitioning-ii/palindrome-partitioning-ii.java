df0c81e944041e3a99336a54d92aef182133ec57
Rashi Smita <rashi.smita2611@gmail.com>
2024-01-30 00:09:25 +0530

    Time: 2523 ms (17.54%) | Memory: 41.1 MB (90.97%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -10,28 +10,24 @@ class Solution {
         }
         return true;
     }
-    public int getMinCuts(int i, int n, String s,int[] dp)
-    {
-        if(i == n)
-            return 0;
-        if(dp[i]!= -1)
-            return dp[i];
-        int mini = Integer.MAX_VALUE;
-        for(int j = i;j<n;j++)
-        {
-            if(isPalindrome(i,j,s))
-            {
-                int cost = 1 + getMinCuts(j+1,n,s,dp);
-                mini = Math.min(mini,cost);
-            }
-        }
-        return dp[i] = mini;
-    }
+
     public int minCut(String s) {
         int n = s.length();
         int[] dp = new int[n+1];
-        for(int i : dp)
-            Arrays.fill(dp,-1);
-        return getMinCuts(0,n,s,dp) - 1;
+        dp[n] = 0;
+        for(int i = n-1;i>=0;i--)
+        {
+            int mini = Integer.MAX_VALUE;
+            for(int j = i;j<n;j++)
+            {
+                if(isPalindrome(i,j,s))
+                {
+                    int cost = 1 + dp[j+1];
+                    mini = Math.min(mini,cost);
+                }
+            }
+            dp[i] = mini;
+        }
+        return dp[0] - 1;
     }
 }
