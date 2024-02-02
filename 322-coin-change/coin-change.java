1d36ee734d73a4e2ef7c9b5ce1eda272d6aa4edd
Rashi Smita <rashi.smita2611@gmail.com>
2024-02-03 02:46:34 +0530

    Time: 108 ms (10.41%) | Memory: 45.1 MB (17.39%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,27 +1,29 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int dp[][] = new int[n][amount+1];
+        int prev[] = new int[amount+1];
         for(int i = 0; i<=amount;i++)
         {
             if(i % coins[0] == 0)
-                dp[0][i] = i/coins[0];
+                prev[i] = i/coins[0];
             else
-                dp[0][i] = (int) Math.pow(10,9);
+                prev[i] = (int) Math.pow(10,9);
         }
 
         for(int i = 1; i<n;i++)
         {
+            int curr[] = new int[amount+1];
             for(int target = 0; target<=amount;target++)
             {
-                int notTake = dp[i-1][target];
+                int notTake = prev[target];
                 int take = (int) Math.pow(10, 9);
                 if(coins[i] <= target)
-                    take = 1 + dp[i][target - coins[i]];
-                dp[i][target] = Math.min(take,notTake);
+                    take = 1 + curr[target - coins[i]];
+                curr[target] = Math.min(take,notTake);
             }
+            prev = curr;
         }
-        int ans = dp[n-1][amount];
+        int ans = prev[amount];
         if(ans >= (int) Math.pow(10, 9))
             return -1;
         return ans;
