3f3d0adb5269f0863f96eb74bbcff008acb9248f
Rashi Smita <rashi.smita2611@gmail.com>
2024-02-03 02:41:05 +0530

    Time: 110 ms (9.94%) | Memory: 44.8 MB (30.00%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -19,9 +19,26 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int dp[][] = new int[n][amount+1];
-        for(int[] row : dp)
-            Arrays.fill(row,-1);
-        int ans = minDenomination(n-1,coins,amount,dp);
+        for(int i = 0; i<=amount;i++)
+        {
+            if(i % coins[0] == 0)
+                dp[0][i] = i/coins[0];
+            else
+                dp[0][i] = (int) Math.pow(10,9);
+        }
+
+        for(int i = 1; i<n;i++)
+        {
+            for(int target = 0; target<=amount;target++)
+            {
+                int notTake = dp[i-1][target];
+                int take = (int) Math.pow(10, 9);
+                if(coins[i] <= target)
+                    take = 1 + dp[i][target - coins[i]];
+                dp[i][target] = Math.min(take,notTake);
+            }
+        }
+        int ans = dp[n-1][amount];
         if(ans >= (int) Math.pow(10, 9))
             return -1;
         return ans;
