92faaf7a77cff6715bdd438fdc557d77c620676f
Rashi Smita <rashi.smita2611@gmail.com>
2024-02-03 02:41:18 +0530

    Time: 109 ms (10.29%) | Memory: 45.3 MB (13.04%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,21 +1,4 @@
 class Solution {
-    public int minDenomination(int i, int[] coins,int amount,int[][] dp)
-    {
-        if(i == 0)
-        {
-            if(amount % coins[0] == 0)
-                return amount/coins[0];
-            else
-                return (int) Math.pow(10, 9);
-        }
-        if(dp[i][amount]!= -1)
-            return dp[i][amount];
-        int notTake = minDenomination(i-1,coins,amount,dp);
-        int take = (int) Math.pow(10, 9);
-        if(coins[i] <= amount)
-            take = 1 + minDenomination(i,coins,amount - coins[i],dp);
-        return dp[i][amount] = Math.min(take,notTake);
-    }
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int dp[][] = new int[n][amount+1];
