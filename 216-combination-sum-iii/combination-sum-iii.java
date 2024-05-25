cd909dd76cd3fe6f5b730904eb4eafce62c5805a
Rashi Smita <rashi.smita2611@gmail.com>
2024-05-25 17:03:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (52.63%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -1,16 +1,18 @@
 class Solution {
-    	public void findCombinations(int num, int length, int target, List<List<Integer>> res, List<Integer> list){
+    public void findCombinations(int num, int length, int target, List<List<Integer>> res, List<Integer> list){
 	if(list.size() == length && target == 0){
 			res.add(new ArrayList<>(list));
             return;
 	}
-	if(num > 9)
-        return;	
 
-	list.add(num);
-	findCombinations(num+1, length, target - num, res, list);
-	list.remove(list.size()-1);
-	findCombinations(num+1, length, target, res, list);
+    for(int i = num;i<=9;i++)
+    {
+        if(i > target)
+            break;
+        list.add(i);
+	    findCombinations(i+1, length, target - i, res, list);
+	    list.remove(list.size()-1);
+    }
 
 }
 	public List<List<Integer>> combinationSum3(int k, int n){
