8f6c0db5cdda0b448f104eb770c2ce3c8ad00e0d
Rashi Smita <rashi.smita2611@gmail.com>
2024-05-26 16:36:23 +0530

    Time: 1 ms (94.43%) | Memory: 44.5 MB (40.06%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,28 +1,30 @@
 class Solution {
-    public void findPermute(int index, int[] nums, List<List<Integer>> res, List<Integer> list, boolean[] hash)
-    {
-        if(index == nums.length)
-        {
-            res.add(new ArrayList<>(list));
-            return;
-        }
-        for(int i = 0; i<nums.length;i++)
-        {
-            if(!hash[i])
-            {
-                list.add(nums[i]);
-                hash[i] = true;
-                findPermute(index+1, nums,res,list,hash);
-                list.remove(list.size() - 1);
-                hash[i] = false;
-            }
-        }
-    }
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        boolean hash[] = new boolean[nums.length];
-        findPermute(0, nums, res,list,hash);
-        return res;
-    }
+    	public void findPermute(int index, int[] nums, List<List<Integer>> res){
+		if(index == nums.length){
+			List<Integer> list = new ArrayList<>();
+			for(int i = 0;i<nums.length;i++){
+				list.add(nums[i]);
+			}
+			res.add(new ArrayList<>(list));
+			return;
+		}
+		for(int i = index; i<nums.length;i++){
+			swap(index,i,nums);
+			findPermute(index+1, nums, res);
+			swap(index,i,nums);
+}
+
+
+	}
+	public void swap(int index, int i, int[] nums){
+		int temp = nums[index];
+		nums[index] = nums[i];
+		nums[i] = temp;
+	}
+	public List<List<Integer>> permute(int[] nums){
+		List<List<Integer>> res = new ArrayList<>();
+		findPermute(0, nums, res);
+		return res;
+	}
+
 }
