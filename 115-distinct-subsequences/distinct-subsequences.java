031d56e3f379617014e59f087549fdeb152aa23c
Rashi Smita <rashi.smita2611@gmail.com>
2024-07-28 20:38:23 +0530

    Time: 13 ms (77.48%) | Memory: 48.9 MB (31.43%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,27 +1,22 @@
 class Solution {
-    public int findDistinctCount(int ind1, int ind2, String s, String t,int[][] dp)
-    {
-        if(ind2 < 0)
-            return 1;
-        if(ind1 < 0)
-            return 0;
-        if(dp[ind1][ind2] != -1)
-            return dp[ind1][ind2];
-        if(s.charAt(ind1) == t.charAt(ind2))
-        {
-            return dp[ind1][ind2] = findDistinctCount(ind1 - 1, ind2 - 1, s, t,dp) + findDistinctCount(ind1 - 1, ind2, s, t,dp);
-        }
-        else
-        {
-            return dp[ind1][ind2] = findDistinctCount(ind1 - 1, ind2, s, t,dp);
-        }
-    }
     public int numDistinct(String s, String t) {
         int m = s.length();
         int n = t.length();
-        int[][] dp = new int[m][n];
-        for(int[] row : dp)
-            Arrays.fill(row,-1);
-        return findDistinctCount(m-1,n-1,s,t,dp);
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 0;i<m;i++)
+            dp[i][0] = 1;
+        for(int i = 1;i<n;i++)
+            dp[0][i] = 0;
+        for(int i = 1;i<=m;i++)
+        {
+            for(int j = 1;j<=n;j++)
+            {
+                if(s.charAt(i-1) == t.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[m][n];
     }
 }
