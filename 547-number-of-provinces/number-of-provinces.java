75bdbc49d4846d6cc877534dac120065875a179f
Rashi Smita <rashi.smita2611@gmail.com>
2025-03-02 16:30:43 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (57.19%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,57 +1,26 @@
-class DisjointSet{
-	ArrayList<Integer> parent = new ArrayList<>();
-	ArrayList<Integer> size = new ArrayList<>();
-	public DisjointSet(int n){
-		for(int i = 0; i<=n;i++)
-		{
-			parent.add(i);
-			size.add(1);
-		}
-	}
-	public int findUltimateParent(int node)
-	{
-		if(parent.get(node) == node)
-			return node;
-		int ulp = findUltimateParent(parent.get(node));
-		parent.set(node, ulp);
-		return parent.get(node);
-	}
-	public void unionBySize(int u, int v)
-	{
-		int ulp_u = findUltimateParent(u);
-		int ulp_v = findUltimateParent(v);
-		if(ulp_u == ulp_v)
-			return;
-		if(size.get(ulp_u) < size.get(ulp_v))
-		{
-			parent.set(ulp_u, ulp_v);
-			size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
-		}
-		else{
-			parent.set(ulp_v, ulp_u);
-			size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
-		}
-	}
-}
-
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        DisjointSet ds = new DisjointSet(isConnected.length);
-        for(int i = 0;i<isConnected.length;i++)
+    public void dfs(int[] visited, int[][] isConnected, int i,int n)
+    {
+        visited[i] = 1;
+        for(int j = 0;j<n;j++)
         {
-            for(int j = 0;j<isConnected[0].length;j++)
+            if(visited[j] == 0 && isConnected[i][j] == 1)
             {
-                if(isConnected[i][j] == 1 && ds.findUltimateParent(i)!= ds.findUltimateParent(j))
-                {
-                    ds.unionBySize(i,j);
-                }
+                dfs(visited,isConnected,j,n);
             }
         }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
         int province = 0;
-        for(int i = 0;i<isConnected.length;i++)
+        int[] visited = new int[n];
+        for(int i = 0;i<n;i++)
         {
-            if(ds.parent.get(i) == i)
+            if(visited[i] == 0)
+            {
                 province++;
+                dfs(visited,isConnected,i,n);
+            }
         }
         return province;
     }
