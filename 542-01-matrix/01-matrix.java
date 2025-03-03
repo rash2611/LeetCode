1356dfe803784f8429ff0e1b243b41fb4e924b42
Rashi Smita <rashi.smita2611@gmail.com>
2025-03-03 18:48:37 +0530

    Time: 14 ms (60.12%) | Memory: 47.2 MB (63.31%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,54 +1,47 @@
-import java.util.*;
-class Pair{
-    int first;
-    int second;
-    int third;
-    Pair(int _first, int _second, int _third)
+class Tuple{
+    int r,c,dist;
+    Tuple(int r,int c, int dist)
     {
-        this.first = _first;
-        this.second = _second;
-        this.third = _third;
+        this.r = r;
+        this.c = c;
+        this.dist = dist;
     }
 }
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int row = mat.length;
-        int column = mat[0].length;
-        int[][] visited = new int[row][column];
-        int[][] distance = new int[row][column];
-        Queue<Pair> q = new LinkedList<>();
+        int col = mat[0].length;
+        int[][] visited = new int[row][col];
+        int[][] distance = new int[row][col];
+        Queue<Tuple> q = new LinkedList<>();
         for(int i = 0;i<row;i++)
         {
-            for(int j = 0;j<column;j++)
+            for(int j = 0; j< col;j++)
             {
                 if(mat[i][j] == 0)
                 {
                     visited[i][j] = 1;
-                    q.add(new Pair(i,j,0));
-                }
-                else
-                {
-                    visited[i][j] = 0;
+                    q.add(new Tuple(i,j,0));
                 }
             }
         }
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
         while(!q.isEmpty())
         {
-            int r = q.peek().first;
-            int c = q.peek().second;
-            int steps = q.peek().third;
-            distance[r][c] = steps;
+            int r = q.peek().r;
+            int c = q.peek().c;
+            int steps = q.peek().dist;
             q.poll();
-            int[] drow = {-1,0,1,0};
-            int[] dcol = {0,1,0,-1};
             for(int i = 0;i<4;i++)
             {
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-                if(nrow>=0 && nrow<row && ncol>=0 && ncol<column && visited[nrow][ncol] == 0)
+                int nrow = drow[i] + r;
+                int ncol = dcol[i] + c;
+                if(nrow >= 0 && nrow < row && ncol >= 0 && ncol < col && visited[nrow][ncol] == 0)
                 {
                     visited[nrow][ncol] = 1;
-                    q.add(new Pair(nrow,ncol,steps+1));
+                    q.add(new Tuple(nrow,ncol,steps + 1));
+                    distance[nrow][ncol] = steps + 1;
                 }
             }
         }
