141b1c15c1a1d54678ee5ab4ae63039816c81c57
Rashi Smita <rashi.smita2611@gmail.com>
2026-02-12 23:42:21 +0530

    Time: 7 ms (27.44%) | Memory: 48.4 MB (79.40%) - LeetSync

---
+++ 252-meeting-rooms/meeting-rooms.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        if(intervals.length == 0)
+            return true;
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        int end = intervals[0][1];
+        for(int i = 1;i<intervals.length;i++)
+        {
+            if(intervals[i][0] < end)
+                return false;
+            end = intervals[i][1];
+        }
+        return true;
+    }
+}
