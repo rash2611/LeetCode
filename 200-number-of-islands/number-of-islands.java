dded77138be8110654ceb8987e547ee94d354139
Rashi Smita <rashi.smita2611@gmail.com>
2026-07-26 13:41:49 +0530

    Time: 4 ms (46.29%) | Memory: 51.6 MB (81.30%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,39 +1,35 @@
 class Solution {
-    public void dfs(int row, int col, int[][] visited, char grid[][], int[] drow, int[] dcol, int m, int n)
+    public void dfs(char[][] grid,int[][] visited, int currrow, int currcol, int row, int col, int[] drow, int[] dcol)
     {
-        visited[row][col] = 1;
-        for(int k = 0; k<4;k++)
+        visited[currrow][currcol] = 1;
+        for(int i = 0;i < 4;i++)
         {
-            int nrow = drow[k] + row;
-            int ncol = dcol[k] + col;
-            if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && visited[nrow][ncol] == 0 && grid[nrow][ncol] == '1')
+            int nrow = currrow + drow[i];
+            int ncol = currcol + dcol[i];
+            if(nrow >=0 && nrow < row && ncol >= 0 && ncol < col && visited[nrow][ncol] == 0 && grid[nrow][ncol] == '1')
             {
-                dfs(nrow, ncol, visited, grid, drow, dcol, m, n);
+                dfs(grid,visited,nrow,ncol,row,col,drow,dcol);
             }
         }
     }
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        if(m == 0)
-            return 0;
-        int n = grid[0].length;
-        if(n == 0)
-            return 0;
-        int component = 0;
-        int[][] visited = new int[m][n];
-        int[] drow = {-1,0,1,0};
-        int[] dcol = {0,1,0,-1};
-        for(int i = 0;i<m;i++)
-        {
-            for(int j = 0;j<n;j++)
+         int m = grid.length;
+         int n = grid[0].length;
+         int island = 0;
+         int visited[][] = new int[m][n];
+         int[] drow = {-1,0,1,0};
+         int[] dcol = {0,1,0,-1};
+         for(int i = 0; i<m;i++)
+         {
+            for(int j = 0; j<n;j++)
             {
                 if(visited[i][j] == 0 && grid[i][j] == '1')
                 {
-                    component++;
-                    dfs(i,j,visited,grid,drow,dcol,m,n);
+                    island++;
+                    dfs(grid,visited,i,j,m,n,drow,dcol);
                 }
             }
-        }
-        return component;
+         }
+         return island;
     }
 }
